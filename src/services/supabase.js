fe0ada9eaa2f73d52e0756323259700092f50df3
Kushagra Bankey <kushagrabankey1703@gmail.com>
2025-05-17 15:40:39 +0530

    chore: new db connection due to inactivity

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://ybslpgfvmgzuujbkzqrr.supabase.co";
+export const supabaseUrl = "https://sfmvdnmoreoniaepubmp.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
